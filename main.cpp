6c407c52f57f75b4fb9ad95f0814202c1d2bcf61
Annonymous Game Developer <pervycreeper@gmail.com>
2012-08-17 07:04:11 -0400

    Frame rate independent movement

--- main.cpp
+++ main.cpp
@@ -13,10 +13,8 @@ double vscale=1;
 
 
 
-//change to gridunits/second; rotations/second
-
-double speed=.02;
-double rotspeed=.005;
+double speedconst =.002;
+double rotspeedconst =.0005;
 
 const int width= 1024;
 const int height = 768;
@@ -26,13 +24,15 @@ double fovconst=.66;
 
 double fov = atan(fovconst)*2.0;
 
-const double twopi = asin(1.0) *4.0;
+const double tau = asin(1.0) *4.0; //yes, I'm trying to be cute
 
-int pixfov = int (double(width) * twopi / fov);
+int pixfov = int (double(width) * tau / fov);
 
-	SDL_Rect screenslice;
-	SDL_Rect screenslice2;
+SDL_Rect screenslice;
+SDL_Rect screenslice2;
 
+
+int frametime[3] = {17,17,16}; //assume 60 fps to start, may cap it there later
 
 
 double playerx = 2.01;
@@ -49,6 +49,7 @@ struct inputstate
 	bool leftstrafe;
 	bool rightstrafe;
 	bool run;
+	bool quit;
 };
 
 inputstate playerstate;
@@ -59,6 +60,8 @@ inputstate playerstate;
 SDL_Surface* renderFrame(SDL_Surface* prescreen, double rposx, double rposy, double directionx, double directiony);
 SDL_Surface* hello = NULL; 
 void drawbackground(double viewdir, SDL_Surface* backimg);
+struct inputstate getstate(struct inputstate curstate);
+bool updateworld ( struct inputstate curstate, double expectedtime);
 
 SDL_Surface* prescreen = NULL; 
 
@@ -109,9 +112,7 @@ int main( int argc, char* args[] )
 
 	//load textures
 	printf("loading textures...");
-	
-//	hello = SDL_LoadBMP( "checker3.bmp" ); //wall texture
-	
+
 	hello = SDL_LoadBMP( "greystone.bmp" );
 
 
@@ -119,137 +120,166 @@ int main( int argc, char* args[] )
 	prescreen = SDL_LoadBMP( "t.bmp" ); //now used to set up a canvas of the right size, fix later
 
 
-//  getinputstate()
-//	updateworld()
+//	int frames = 0;
 
-	int frames = 3;
-
-	struct inputstate curstate = {false, false, false, false, false, false, false};
-	struct inputstate oldstate = curstate;
-	struct inputstate nextstate = curstate;
+	struct inputstate curstate = {false, false, false, false, false, false, false, false};
+	struct inputstate oldstate;
+  
 
 	bool mainloopdone = false;
+	long long a = SDL_GetTicks();
 
 	while (!mainloopdone)
 	{
-		SDL_Event event;
-		while (SDL_PollEvent(&event))
-		{
-			switch(event.type){  /* Process the appropiate event type */
-			case SDL_KEYDOWN:
-				{
-					if (event.key.keysym.sym  ==  SDLK_ESCAPE){
-						mainloopdone = true;}
-
-					if ((event.key.keysym.sym  == SDLK_w) | (event.key.keysym.sym  == SDLK_UP)){
-						curstate.up= true;
-					}
-					if ((event.key.keysym.sym  == SDLK_s) | (event.key.keysym.sym  == SDLK_DOWN)){
-						curstate.down= true;}
-
-					if (event.key.keysym.sym  == SDLK_a){
-						curstate.leftstrafe= true;}
-
-					if (event.key.keysym.sym  == SDLK_d){
-						curstate.rightstrafe= true;}
-					if (event.key.keysym.sym  == SDLK_LEFT){
-					curstate.left= true;
-						
-					}					
-					if (event.key.keysym.sym  == SDLK_RIGHT){
-				curstate.right= true;
-					}
-					break;
-				}
-
-				case SDL_KEYUP:
-				{
-
-					if ((event.key.keysym.sym  == SDLK_w) | (event.key.keysym.sym  == SDLK_UP)){
-						curstate.up= false;
-					}
-					if ((event.key.keysym.sym  == SDLK_s) | (event.key.keysym.sym  == SDLK_DOWN)){
-						curstate.down= false;}
-
-					if (event.key.keysym.sym  == SDLK_a){
-						curstate.leftstrafe= false;}
-
-					if (event.key.keysym.sym  == SDLK_d){
-						curstate.rightstrafe= false;}
-					if (event.key.keysym.sym  == SDLK_LEFT){
-					curstate.left= false;
-						
-					}					
-					if (event.key.keysym.sym  == SDLK_RIGHT){
-				curstate.right= false;
-					}
-					break;
-				}
-
-
-			case SDL_QUIT:
-				mainloopdone= true;
-				break;
-			default:
-				break;
-			}											
-		}
-
-
-
-		if (curstate.up== true){
-			playerx += speed * cos(playerangle);
-			playery += speed * sin(playerangle);}
-
-
-		if (curstate.down== true){
-			playerx -= speed * cos(playerangle);
-			playery -= speed * sin(playerangle);}
-
-		if (curstate.leftstrafe== true){
-			playerx += speed * sin(playerangle);
-			playery -= speed * cos(playerangle);}
-
-		if (curstate.rightstrafe== true){
-			playerx -= speed * sin(playerangle);
-			playery += speed * cos(playerangle);}
-		if (curstate.left== true){
-			playerangle-=rotspeed;
-			if (playerangle<0.0){
-				playerangle += twopi;
-			}
-		}					
-		if (curstate.right== true){
-			playerangle+=rotspeed;
-			if (playerangle>=twopi){
-				playerangle -= twopi;
-			}
-		}
-
-			// change!!!!!
-//	curstate= oldstate;
-
-
-
-
+		oldstate = curstate;
+		curstate = getstate(oldstate);
+		double expectedtime = (frametime[0] + frametime[1] +frametime [2]) / 3.0 ;
+		mainloopdone = updateworld (curstate, expectedtime); // if player wants to quit, updateworld will return true
 		drawbackground(playerangle, pano);
-		long long a = SDL_GetTicks(); //time frame
 		prescreen = renderFrame(prescreen, playerx , playery, cos(playerangle) , sin(playerangle));
-		printf("%d, %d, %d\n", (SDL_GetTicks()-a), frames ); //time frame
+		frametime [(framecount % 3)] = SDL_GetTicks()-a;
+		a = SDL_GetTicks(); 
+		printf("%d, %d, %d, %f, %d FPS\n", frametime[0], framecount, expectedtime, int(1000.0/expectedtime) ); //time frame
 
 		//finally, draw screen
 		SDL_BlitSurface(prescreen , NULL, screen, NULL );
 		SDL_Flip( screen );
-		frames++;
+		framecount++;
 	}
 	SDL_FreeSurface( prescreen );
 	SDL_Quit(); 
 	return 0; 
 }
 
+struct inputstate getstate(struct inputstate curstate ){
+
+	/*there is a design flaw here, where since multiple keys can have the 	
+	same effect, lifting one while the other is depressed will result in 
+	nullifying the effect of the other one. Will not fix for time being.
+	*/
+
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event))
+	{
+		switch(event.type){
+		case SDL_KEYDOWN:
+			{
+				if (event.key.keysym.sym  ==  SDLK_ESCAPE){
+					curstate.quit = true;}
+				if ((event.key.keysym.sym  == SDLK_w) | (event.key.keysym.sym  == SDLK_UP)){
+					curstate.up= true;
+				}
+				if ((event.key.keysym.sym  == SDLK_s) | (event.key.keysym.sym  == SDLK_DOWN)){
+					curstate.down= true;}
+
+				if (event.key.keysym.sym  == SDLK_a){
+					curstate.leftstrafe= true;}
+
+				if (event.key.keysym.sym  == SDLK_d){
+					curstate.rightstrafe= true;}
+				if (event.key.keysym.sym  == SDLK_LEFT){
+					curstate.left= true;
+
+				}					
+				if (event.key.keysym.sym  == SDLK_RIGHT){
+					curstate.right= true;
+				}
+				break;
+			}
+
+		case SDL_KEYUP:
+			{
+
+				if ((event.key.keysym.sym  == SDLK_w) | (event.key.keysym.sym  == SDLK_UP)){
+					if (curstate.up == true){
+						curstate.up= false;}
+				}
+				if ((event.key.keysym.sym  == SDLK_s) | (event.key.keysym.sym  == SDLK_DOWN)){
+
+					if (curstate.down== true){
+						curstate.down= false;}
+				}
+				if (event.key.keysym.sym  == SDLK_a){
+					if (curstate.leftstrafe== true){
+
+
+						curstate.leftstrafe= false;}}
+
+				if (event.key.keysym.sym  == SDLK_d){
+					if (curstate.rightstrafe==true){
+
+						curstate.rightstrafe= false;}}
+				if (event.key.keysym.sym  == SDLK_LEFT){
+
+					if (curstate.left== true){
+						curstate.left= false;}
+
+				}					
+				if (event.key.keysym.sym  == SDLK_RIGHT){
+					if (curstate.right==true){
+						curstate.right= false;}
+				}
+				break;
+			}
+
+
+		case SDL_QUIT:
+			curstate.quit= true;
+			break;
+		default:
+			break;
+		}											
+	}
+	return curstate;
+}
+
+bool updateworld ( struct inputstate curstate, double expectedtime){
+	double runconst;
+	if (curstate.run== true){
+		runconst = 1.0;}
+	else{
+		runconst = 3.0;}
+
+	double speed = speedconst * runconst * expectedtime ;
+	double rotspeed = rotspeedconst * runconst *expectedtime;
+	if (curstate.quit== true){
+		return true;}
+
+	if (curstate.up== true){
+		playerx += speed * cos(playerangle);
+		playery += speed * sin(playerangle);}
+
+
+	if (curstate.down== true){
+		playerx -= speed * cos(playerangle);
+		playery -= speed * sin(playerangle);}
+
+	if (curstate.leftstrafe== true){
+		playerx += speed * sin(playerangle);
+		playery -= speed * cos(playerangle);}
+
+	if (curstate.rightstrafe== true){
+		playerx -= speed * sin(playerangle);
+		playery += speed * cos(playerangle);}
+	if (curstate.left== true){
+		playerangle-=rotspeed;
+		if (playerangle<0.0){
+			playerangle += tau;
+		}
+	}					
+	if (curstate.right== true){
+		playerangle+=rotspeed;
+		if (playerangle>=tau){
+			playerangle -= tau;
+		}
+	}
+	return false;
+}
+
 void drawbackground(double viewdir, SDL_Surface* backimg){
 
-	if ( viewdir + fov < twopi){
+	if ( viewdir + fov < tau){
 		screenslice.x =int(width*viewdir/fov);
 		screenslice.y =0;
 		screenslice.w = width;
@@ -260,19 +290,19 @@ void drawbackground(double viewdir, SDL_Surface* backimg){
 	{
 		screenslice.x =int(width*viewdir/fov);
 		screenslice.y =0;
-		screenslice.w = int(width*(twopi-viewdir)/fov);
+		screenslice.w = int(width*(tau-viewdir)/fov);
 		screenslice.h = height;
 
 		SDL_BlitSurface(backimg , &screenslice   , prescreen, NULL );
 
-		screenslice2.x =int(width*(twopi-viewdir)/fov);
+		screenslice2.x =int(width*(tau-viewdir)/fov);
 		screenslice2.y =0;
-		screenslice2.w = width -int(width*(twopi-viewdir)/fov);
+		screenslice2.w = width -int(width*(tau-viewdir)/fov);
 		screenslice2.h = height;
 
 		screenslice.x =0;
 		screenslice.y =0;
-		screenslice.w = width -int(width*(twopi-viewdir)/fov);
+		screenslice.w = width -int(width*(tau-viewdir)/fov);
 		screenslice.h = height;
 
 		SDL_BlitSurface(backimg , &screenslice   , prescreen, &screenslice2 );
@@ -360,7 +390,7 @@ SDL_Surface* renderFrame(SDL_Surface* canvas, double rposx, double rposy, double
 		// originally, scaledrawheight=int(vscale*height/d);
 		// keep it even to make seams between pixels look better-- doesn't fix middle of line of screen by itself
 		scaledrawheight=2*int(vscale*height/d/2);
-		
+
 
 
 		if (height < scaledrawheight) {
@@ -371,11 +401,11 @@ SDL_Surface* renderFrame(SDL_Surface* canvas, double rposx, double rposy, double
 		}
 
 		startdraw = int((height-drawheight)/2);
-		
+
 		h=32.0*(scaledrawheight-drawheight)/scaledrawheight;
 
 		s=63.999999/scaledrawheight; //rather than 64.0-- a fudge factor to keep the middle row free from noise due to rounding
-		
+
 		if (hitx){ 
 			hit=int(64*(intercepty-int(intercepty)));
 		}
@@ -383,11 +413,11 @@ SDL_Surface* renderFrame(SDL_Surface* canvas, double rposx, double rposy, double
 			hit=int(64*(interceptx-int(interceptx)));
 
 		} 
-		
+
 		// this is a mess due to recasting, but not sure how to change, since this is how SDL works.
 		// moved locking/unlocking to parent loop, but not sure if this is a good idea. calls sqrt() while locked
 		// todo: add facilities for different textures, 
-//		SDL_LockSurface (canvas) ;
+		//		SDL_LockSurface (canvas) ;
 		int canvpitch = canvas->pitch;
 		int c = (curcol)* canvas->format->BytesPerPixel;
 		void *canvaspixels = canvas->pixels;
@@ -399,14 +429,13 @@ SDL_Surface* renderFrame(SDL_Surface* canvas, double rposx, double rposy, double
 			*(Uint32 *)j = *(Uint32 *)((int(h))*(texpitch)+k);
 			h += s;
 		}
-//		SDL_UnlockSurface (canvas);
+		//		SDL_UnlockSurface (canvas);
 		lookrayx += camstepx;
 		lookrayy += camstepy;
 	}
 	SDL_UnlockSurface (canvas);
 	return canvas;
 }
-
 
 
 
